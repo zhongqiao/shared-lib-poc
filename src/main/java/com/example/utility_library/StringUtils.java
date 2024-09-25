980b3525c534c57369ede29598e4bc51da6e19b9
Zhang <victor.q.zhang@accenture.com>
2024-09-25 14:12:47 -0400

    sample code for pulishing the lib using GitHub package

---
+++ src/main/java/com/example/utility_library/StringUtils.java
@@ -0,0 +1,11 @@
+package com.example.utility_library;
+
+public class StringUtils {
+
+    public static String capitalize(String input) {
+        if (input == null || input.isEmpty()) {
+            return input;
+        }
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+    }
+}
---
+++ src/main/java/com/example/utility_library/UtilityLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.utility_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityLibraryApplication.class, args);
+	}
+
+}
